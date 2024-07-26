9cf8fa03b4d2d0697384e08a6e4d0a7b0cb28c4e
WERQ <workaferaww@gmail.com>
2024-07-26 16:11:23 +0300

    Create weather_advice.py
    I created a python script recommending what to wear on different weather.

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+the_weather = input("What's the weather like today? (sunny/rainy/cold):")
+
+if the_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif the_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif the_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
